8cc51c553d4507890a5a01e81781e650c446bd62
sripathinimmani <sripathi.nimmani@gmail.com>
2026-07-14 01:57:19 +0000

    Save my latest changes

---
+++ frontend/src/app/components/apply-leave/apply-leave.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apply-leave',
+  imports: [],
+  templateUrl: './apply-leave.html',
+  styleUrl: './apply-leave.scss',
+})
+export class ApplyLeave {}
---
+++ frontend/src/app/components/leave-list/leave-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leave-list',
+  imports: [],
+  templateUrl: './leave-list.html',
+  styleUrl: './leave-list.scss',
+})
+export class LeaveList {}
